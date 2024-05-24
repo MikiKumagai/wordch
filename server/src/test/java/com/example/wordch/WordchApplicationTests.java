4ffdc1c966a8547a46b2f204543fb48334321484
Kumagai Miki <kumagai@tierline.com>
2024-05-24 15:59:26 +0900

    add springboot

---
+++ server/src/test/java/com/example/wordch/WordchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wordch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
