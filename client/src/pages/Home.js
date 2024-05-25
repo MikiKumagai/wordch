14c69d2dd8e5518eacce3a84e20a1ac9b699ec14
Kumagai Miki <kumagai@tierline.com>
2024-05-25 16:17:39 +0900

    connect websocket

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,50 @@
-import { Form, Button, Card, Container, Row, Col } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { Button, Container, Row, Col } from 'react-bootstrap';
+import useStompClient from './../common/useStompClient';
 
 export default function Home() {
+  const [playerAmount, setPlayerAmount] = useState(0);
+  const [dealerAmount, setDealerAmount] = useState(0);
+  const { connect, disconnect, connected, stompClient } = useStompClient('http://localhost:8080/gs-guide-websocket', 'YOUR_TOKEN');
+
+  useEffect(() => {
+    connect();
+    return () => {
+      disconnect();
+    };
+  }, [connect, disconnect]);
+
+  const clickPlayer = () => {
+    stompClient.send('/app/hello', {}, JSON.stringify({ 'name': 'player' }));
+    setPlayerAmount(playerAmount + 1);
+  };
+
+  const clickDealer = () => {
+    stompClient.send('/app/hello', {}, JSON.stringify({ 'name': 'dealer' }));
+    setDealerAmount(dealerAmount + 1);
+  };
+
   return (
     <Container>
-      <Button variant='light' href="/player">player</Button>
-      <Button variant='light' href="/dealer">dealer</Button>
+      <Row>
+        <Col>
+          <Button variant='light' type="button" onClick={clickPlayer}>player</Button>
+        </Col>
+        <Col>
+          <h1>player: {playerAmount}</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button variant='light' type="button" onClick={clickDealer}>dealer</Button>
+        </Col>
+        <Col>
+          <h1>dealer: {dealerAmount}</h1>
+        </Col>
+      </Row>
+      <div>
+        Connection status: {connected ? 'Connected' : 'Disconnected'}
+      </div>
     </Container>
   );
 }
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,46 +1,26 @@
 import { Button, Card, Container } from 'react-bootstrap';
-import { useState } from 'react';
-import { Client } from '@stomp/stompjs'; 
-import SockJS from 'sockjs-client';
+import { useNavigate } from 'react-router-dom';
+import useStompClient from './../common/useStompClient';
 
 export default function Landing() {
-  const [stompClient, setStompClient] = useState(null);
+  const navigate = useNavigate();
   const YOUR_TOKEN = 'YOUR_TOKEN';
+  const { connect, disconnect, connected } = 
+  useStompClient('http://localhost:8080/gs-guide-websocket', YOUR_TOKEN);
 
-  const connect = () => {
-    const socket = new SockJS('http://localhost:8080/gs-guide-websocket', {}, 
-    { headers: { 'Authorization': 'Bearer ' + YOUR_TOKEN } });
-    const client = new Client({
-      webSocketFactory: () => socket,
-      debug: (str) => {
-        console.log(str);
-      }
-    });
-    client.onConnect = (frame) => {
-      console.log('Connected: ' + frame);
-    };
-    client.onStompError = (frame) => {
-      console.error('Broker reported error: ' + frame.headers['message']);
-      console.error('Additional details: ' + frame.body);
-    };
-    client.activate();
-    setStompClient(client);
-  };
-
-  const disconnect = () => {
-    if (stompClient !== null) {
-      stompClient.deactivate();
-    }
-    console.log("Disconnected");
-  };
-  
   return (
     <Container>
       <Card>
         <Card.Body>
-        <Button type="submit" variant="secondary"  onClick={()=>connect()}>Connect</Button>
-        <Button type="submit" variant="secondary" onClick={()=>disconnect()}>Disconnect</Button>
-
+          <Button type="button" variant="secondary" onClick={() => { connect(); navigate('/home'); }}>
+            Connect
+          </Button>
+          <Button type="button" variant="secondary" onClick={() => disconnect()}>
+            Disconnect
+          </Button>
+          <div>
+            Connection status: {connected ? 'Connected' : 'Disconnected'}
+          </div>
           {/* <FormProvider {...hookForm}>
             <form onSubmit={handleSubmit(onSubmit)}>
               <Stack direction="horizontal" gap={3}>
